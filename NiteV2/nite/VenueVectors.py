aa1e3a5e0d218393525762f3bb78d8a7fcb2d3c7
benlongcroft <blongcroft@gmail.com>
2020-11-07 13:12:46 +0000

    converted all addresses in ClubDataDB.db to coordinates to increase speed of finding potential vectors

--- NiteV2/nite/VenueVectors.py
+++ NiteV2/nite/VenueVectors.py
@@ -2,7 +2,6 @@ import datetime
 import sqlite3
 from collections import namedtuple
 from geopy.distance import geodesic
-import requests
 import pandas as pd
 
 class GetVenueVectors:
@@ -33,44 +32,40 @@ class GetVenueVectors:
             print(e)
 
     @staticmethod
-    def GetDistanceBetweenAddress(current_location, y_address):
-        geo_url = 'https://maps.googleapis.com/maps/api/geocode/json'
-        api_key = "AIzaSyAAQ2bYW5ZsFg3VUoms-BxkeO5NCafGu5o"
-        #use google maps API
-        response = requests.get(geo_url, params={'address':y_address, 'key':api_key})
-        #insert address into requests module
-        results = response.json()['results'][0]
-        y_coordinates = results['geometry']['location']
-        #get coordinates from API response
+    def GetDistanceBetweenAddress(current_location, y_coordinates):
         #use geopy geodesic module to get distance in miles and return it to function call
         return geodesic((y_coordinates['lat'], y_coordinates['lng']), (current_location['lat'], current_location['lng'])).miles
 
 
 
     def GetValidVenues(self):
-        dbObj = sqlite3.connect('./ClubDataDB.db') #connect to database
-        cursorObj = dbObj.cursor()
+        db_obj = sqlite3.connect('./ClubDataDB.db') #connect to database
+        cursor_obj = db_obj.cursor()
         command = '''SELECT * FROM venues WHERE dress_rating <= ?''' #generate base command
+
         if self.TwentyOnePlus == True:
             command = command + ''' AND age_restriction = "over 21s"'''
         if self.paid == False:
             command = command + ''' AND entry_price = no door charge'''
 
-        cursorObj.execute(command, (self.smartness,)) #execute command
+        cursor_obj.execute(command, (self.smartness,)) #execute command
 
         fields = ["venue_id", "name", "description", "venue_type",
-                                              "age_restriction", "entry_price", "dress_code",
-                                              "dress_rating", "address", "distance"]
-        ValidVenuesDF = pd.DataFrame(columns=fields) #create pandas dataframe
-        for record in cursorObj.fetchall():
+                "age_restriction", "entry_price", "dress_code",
+                "dress_rating", "address", "distance"]
+
+        valid_venues_df = pd.DataFrame(columns=fields) #create pandas dataframe
+
+        for record in cursor_obj.fetchall():
             record = list(record) #turn record from tuple to list
-            address = record[-1]
-            distance = self.GetDistanceBetweenAddress(self.location, address) #get distance between address and venues
+            record_coordinates = {'lat':record[-1].split(",")[0], 'lng':record[-1].split(",")[1]} #get coordinates and split them into a dictionary
+            distance = self.GetDistanceBetweenAddress(self.location, record_coordinates) #get distance between address and venues
             record.append(distance)
-            record = pd.DataFrame([record], columns = fields)
-            ValidVenuesDF = ValidVenuesDF.append(record, ignore_index=True) #add series to pandas df
+            record = pd.DataFrame([record], columns = fields) #turn record into df so it can be appended
+            valid_venues_df = valid_venues_df.append(record, ignore_index=True) #add series to pandas df
+
         pd.set_option('display.max_columns', None)
-        print(len(ValidVenuesDF.axes[0]))
+        print(len(valid_venues_df.axes[0]))
 
 
 
