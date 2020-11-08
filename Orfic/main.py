877488604e7e736c1e21d8fd5d3c4712262536f6
benlongcroft <blongcroft@gmail.com>
2020-11-08 16:03:41 +0000

    created vector retrieval system in GetVenueVectors called Get() which adds a column to the pd df of the matching vectors to the venues

--- Orfic/main.py
+++ Orfic/main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
 from orfic.cli import cli
+from K2K.main import GetClosestVectors, ConvertKeywordsToVectors
 import sqlite3
 
 if __name__ == '__main__':
     VenueObj = cli()
     db_obj = sqlite3.connect('./ClubDataDB.db')  # connect to database
     cursor_obj = db_obj.cursor()
-    AllValidVenues = VenueObj.GetValidVenues(None, db_obj, cursor_obj)
+    valid_venues_df = VenueObj.FetchValidVenues(None, cursor_obj)
+    vector_df = VenueObj.Get(valid_venues_df, cursor_obj)
+    print(vector_df.head())
+    # valid_venue_ids = valid_venues_df['venue_id']
