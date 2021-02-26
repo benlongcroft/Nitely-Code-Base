44e2e2a90568705891b46c74bede2550c67ef0d7
benlongcroft <blongcroft@gmail.com>
2021-02-26 14:02:30 +0000

    updated documentation for main

--- __main__.py
+++ __main__.py
@@ -4,14 +4,17 @@ from vector_k2k import K2K
 
 
 def main(kwargs):
+    """
+    Main NITE program - what is called when a new session is to be created for a user
+    :param kwargs: commandline input from cli argparser
+    :return: NITE's as package objects
+    """
     nite_obj = start_NITE(kwargs)
     user_obj = nite_obj.get_user()
     k2k_obj = K2K(user_obj.get_keywords, user_obj.get_weightings)
     user_vector = user_obj.get_user_vector(k2k_obj)
     venues = nite_obj.get_nearby_venues()
     venue_similarity = nite_obj.get_similarity(venues, user_vector)
-    print(repr(venue_similarity))
-    # TODO: implement this method - return dictionary {venue : similarity}
     nite_obj.create_package(venues, venue_similarity)
 
 
